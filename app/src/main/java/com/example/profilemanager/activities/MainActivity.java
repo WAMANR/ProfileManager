6e7e295b40802ba162afa72f3f2c2d7ab0dab3e6
Adil ROUICHI <adil.rouichi011@gmail.com>
2021-12-04 00:08:48 +0100

    add QRScanner and Main buttons + logout

--- app/src/main/java/com/example/profilemanager/activities/MainActivity.java
+++ app/src/main/java/com/example/profilemanager/activities/MainActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.profilemanager.R;
 import com.example.profilemanager.databinding.ActivityMainBinding;
+import com.example.profilemanager.utilities.Constants;
 import com.example.profilemanager.utilities.PreferenceManager;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,7 +26,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setListeners() {
-        binding.imageScanner.setOnClickListener(v ->
+       binding.icLogout.setOnClickListener(v -> binding.logoutConfirmation.setVisibility(View.VISIBLE));
+       binding.logoutNo.setOnClickListener(v -> binding.logoutConfirmation.setVisibility(View.INVISIBLE));
+       binding.logoutYes.setOnClickListener(v -> {
+           preferenceManager.putBoolean(Constants.KEY_IS_SIGNED_IN, false);
+           Intent intent = new Intent(getApplicationContext(), SignInActivity.class);
+           startActivity(intent);
+           finish();
+       });
+       binding.icScanner.setOnClickListener(v ->
                 startActivity(new Intent(getApplicationContext(), ScannerActivity.class)));
     }
 
