fa01223ea517989acddb5988d793eae8928de9f7
Adil ROUICHI <adil.rouichi011@gmail.com>
2021-12-03 18:26:59 +0100

    sign in and sign up implementation

--- app/src/main/java/com/example/profilemanager/activities/SignInActivity.java
+++ app/src/main/java/com/example/profilemanager/activities/SignInActivity.java
@@ -1,46 +1,104 @@
 package com.example.profilemanager.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
 import android.widget.Toast;
 
-import com.example.profilemanager.R;
-import com.example.profilemanager.databinding.ActivitySignInBinding;
-import com.google.firebase.firestore.FirebaseFirestore;
+import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.HashMap;
+import com.example.profilemanager.MainActivity;
+import com.example.profilemanager.activities.SignUpActivity;
+import com.example.profilemanager.databinding.ActivitySignInBinding;
+import com.example.profilemanager.utilities.Constants;
+import com.example.profilemanager.utilities.PreferenceManager;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class SignInActivity extends AppCompatActivity {
 
     private ActivitySignInBinding binding;
+    private PreferenceManager preferenceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        if(preferenceManager.getBoolean(Constants.KEY_IS_SIGNED_IN)){
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
         binding = ActivitySignInBinding.inflate(getLayoutInflater());
-        setContentView(R.layout.activity_sign_in);
-        setListeners();
-        addDataToFirestore();
+        setContentView(binding.getRoot());
+        setListerners();
     }
 
-    private void setListeners(){
-        //binding.element.setOnClickListerner(v -> {});
+    private void setListerners(){
+
+        binding.textCreateNewAccount.setOnClickListener(v ->
+                startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
+        binding.buttonSignIn.setOnClickListener(v -> {
+            if(isValidSignInDetails()){
+                signIn();
+            }
+        });
     }
 
-    private void addDataToFirestore(){
+    private void signIn(){
+        loading(true);
         FirebaseFirestore database = FirebaseFirestore.getInstance();
-        HashMap<String, Object> data = new HashMap<>();
-        data.put("first_name", "Théo");
-        data.put("last_name", "Montaigu");
-        database.collection("users")
-                .add(data)
-                .addOnSuccessListener(documentReference -> {
-                    Toast.makeText(getApplicationContext(), "Data Inserted", Toast.LENGTH_SHORT).show();
-                })
-                .addOnFailureListener(exception -> {
-                    Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_SHORT).show();
+        database.collection(Constants.KEY_COLLECTION_USERS)
+                .whereEqualTo(Constants.KEY_EMAIL, binding.inputEmail.getText().toString())
+                .whereEqualTo(Constants.KEY_PASSWORD, binding.inputPassword.getText().toString())
+                .get()
+                .addOnCompleteListener(task -> {
+                    if(task.isSuccessful() && task.getResult() != null && task.getResult().getDocuments().size() > 0){
+                        DocumentSnapshot documentSnapshot = task.getResult().getDocuments().get(0);
+                        preferenceManager.putBoolean(Constants.KEY_IS_SIGNED_IN, true);
+                        preferenceManager.putString(Constants.KEY_USER_ID, documentSnapshot.getId());
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                    }
+                    else{
+                        loading(false);
+                        showToast("Unable to sign in");
+                    }
                 });
 
+    }
+
+    private void loading(Boolean isLoading){
+        if(isLoading){
+            binding.buttonSignIn.setVisibility(View.INVISIBLE);
+            binding.progressBar.setVisibility(View.VISIBLE);
+        }
+        else{
+            binding.buttonSignIn.setVisibility(View.VISIBLE);
+            binding.progressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void showToast(String message){
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    private Boolean isValidSignInDetails(){
+        if(binding.inputEmail.getText().toString().trim().isEmpty()){
+            showToast("Enter email");
+            return false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(binding.inputEmail.getText().toString()).matches()){
+            showToast("Enter valid email");
+            return false;
+        }
+        else if (binding.inputPassword.getText().toString().trim().isEmpty()){
+            showToast("Enter password");
+            return false;
+        }
+        else{
+            return true;
+        }
     }
 }
--- app/src/main/java/com/example/profilemanager/activities/SignUpActivity.java
+++ app/src/main/java/com/example/profilemanager/activities/SignUpActivity.java
@@ -2,25 +2,108 @@ package com.example.profilemanager.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Toast;
 
-import com.example.profilemanager.R;
-import com.example.profilemanager.databinding.ActivitySignInBinding;
+import com.example.profilemanager.MainActivity;
 import com.example.profilemanager.databinding.ActivitySignUpBinding;
+import com.example.profilemanager.utilities.Constants;
+import com.example.profilemanager.utilities.PreferenceManager;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 
 public class SignUpActivity extends AppCompatActivity {
 
     private ActivitySignUpBinding binding;
+    private PreferenceManager preferenceManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivitySignUpBinding.inflate(getLayoutInflater());
-        setContentView(R.layout.activity_sign_up);
-        setListeners();
+        setContentView(binding.getRoot());
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        setListerners();
     }
 
-    private void setListeners(){
-        //binding.element.setOnClickListerner(v -> {});
+    private void setListerners(){
+        binding.textSignIn.setOnClickListener(v -> onBackPressed());
+        binding.buttonSignUp.setOnClickListener(v -> {
+            if(isValidSignUpDetails()){
+                signUp();
+            }
+        });
     }
+
+    private void showToast(String message){
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void signUp(){
+        loading(true);
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        HashMap<String, Object> user = new HashMap<>();
+        user.put(Constants.KEY_EMAIL, binding.inputEmail.getText().toString());
+        user.put(Constants.KEY_PASSWORD, binding.inputPassword.getText().toString());
+        database.collection(Constants.KEY_COLLECTION_USERS)
+                .add(user)
+                .addOnSuccessListener(documentReference -> {
+                    loading(false);
+                    preferenceManager.putBoolean(Constants.KEY_IS_SIGNED_IN, true);
+                    preferenceManager.putString(Constants.KEY_USER_ID, documentReference.getId());
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                })
+                .addOnFailureListener(exception -> {
+                    Log.d("signupDatabase : ", "Fail: ");
+                });
+
+    }
+
+    private Boolean isValidSignUpDetails(){
+
+        if(binding.inputEmail.getText().toString().trim().isEmpty()){
+            showToast("Enter email");
+            return false;
+        }
+        else if(!Patterns.EMAIL_ADDRESS.matcher(binding.inputEmail.getText().toString()).matches()){
+            showToast("Enter valid email");
+            return false;
+        }
+        else if(binding.inputPassword.getText().toString().trim().isEmpty()){
+            showToast("Enter password");
+            return false;
+        }
+        else if(binding.inputConfirmPassword.getText().toString().trim().isEmpty()){
+            showToast("Confirm your password");
+            return false;
+        }
+        else if(!binding.inputPassword.getText().toString().equals(binding.inputConfirmPassword.getText().toString())){
+            showToast("Password and confirm password aren't the same");
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    private void loading(Boolean isloading){
+        if(isloading){
+            binding.buttonSignUp.setVisibility(View.INVISIBLE);
+            binding.progressBar.setVisibility(View.VISIBLE);
+        }
+        else{
+            binding.progressBar.setVisibility(View.INVISIBLE);
+            binding.buttonSignUp.setVisibility(View.VISIBLE);
+        }
+    }
+
+
 }
