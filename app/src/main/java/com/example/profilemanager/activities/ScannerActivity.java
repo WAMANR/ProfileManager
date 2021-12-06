56b6ba32ce0d3512f09ce2a348cd893a04e32b0e
Inepsie <theo.montaigu@gmail.com>
2021-12-06 10:47:49 +0100

    Scanner

--- app/src/main/java/com/example/profilemanager/activities/ScannerActivity.java
+++ app/src/main/java/com/example/profilemanager/activities/ScannerActivity.java
@@ -1,17 +1,31 @@
 package com.example.profilemanager.activities;
+//package net.smallacademy.qrapp;
 
+
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.profilemanager.databinding.ActivityScannerBinding;
 import com.example.profilemanager.utilities.PreferenceManager;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
+
 
 
 public class ScannerActivity extends AppCompatActivity {
 
     private ActivityScannerBinding binding;
     private PreferenceManager preferenceManager;
+    CodeScanner codeScanner;
+    CodeScannerView scannerView;
+    TextView resultData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +34,21 @@ public class ScannerActivity extends AppCompatActivity {
         binding = ActivityScannerBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setListeners();
+        scannerView = binding.scannerView;
+        codeScanner = new CodeScanner(this, scannerView);
+        resultData = binding.resultsOfQr;
+
+        codeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull Result result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        resultData.setText(result.getText());
+                    }
+                });
+            }
+        });
     }
 
     private void setListeners() {
@@ -28,4 +57,6 @@ public class ScannerActivity extends AppCompatActivity {
             finish();
         });
     }
+
+
 }
